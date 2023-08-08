e83f9ca554c3a2841a5b3ae0793496b4862338dd
gemma1987 <gemma.neeleman@gmail.com>
2023-08-08 13:53:58 +0200

    changed myGame to StartedGame and took out all errors, code not yet proper for what I need to do

--- src/main/java/com/boardgameApp/Repositories/StartedGameRepository.java
+++ src/main/java/com/boardgameApp/Repositories/StartedGameRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface StartedGameRepository extends JpaRepository<StartedGame, int> {
+public interface StartedGameRepository extends JpaRepository<StartedGame, Integer> {
     Optional<List<StartedGame>> findAllByUserUserID(String userID);
 
 }
